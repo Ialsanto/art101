3bc44c5cc7b869917e864376c9874e5ce0477011
Ialsanto <ialsanto@ucsc.edu>
2024-04-23 16:21:21 -0700

    added .html files for lab4 and pseudocode in the lab.js

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,19 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Checkers Implementation, ART 101 
+// Author: Ian Santos
+// Date: April 22 2024
+
+// Checkers: 
+// 		|Allow user Options
+//        (player vs player, player vs computer)
+// 		|Print the board
+//            Position of all pieces
+//            Remove pieces if needed 
+// 		|Accept user input 
+//        (red or black) 
+// 		|validate move
+//        (forward if space, diagonal if opponent is on the diagonal)
+// 		|(pvp) check if game is won(give msg) or jump to step 2, 
+//        (PvC)check if game is won(give msg) or make computer make a move
 
 // Constants
 
